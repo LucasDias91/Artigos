71642031e2f0212887f3fd7e43e97ede1dc5a90c
Josué Dall'Agnese <josue.agnese@totvs.com.br>
2019-05-31 13:44:57 -0300

    ApiVersioning

---
+++ ApiVersioning/ApiVersioning/Startup.cs
@@ -0,0 +1,53 @@
+﻿using ApiVersioning.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiVersioning
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddApiVersioning(o =>
+            {
+                o.UseApiBehavior = false;
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+
+                o.ApiVersionReader = ApiVersionReader.Combine(
+                    new HeaderApiVersionReader("x-api-version"),
+                    new QueryStringApiVersionReader(),
+                    new UrlSegmentApiVersionReader());
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddSwaggerGen();
+            services.ConfigureOptions<ConfigureSwaggerOptions>();
+        }
+
+        public void Configure(IApplicationBuilder app, IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ ApiVersioning/ApiVersioning/Swagger/SwaggerVersioningOperationFilter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace ApiVersioning.Swagger
+{
+    public class SwaggerVersioningOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            var apiDescription = context.ApiDescription;
+
+            operation.Deprecated = apiDescription.IsDeprecated();
+
+            if (operation.Parameters == null)
+            {
+                return;
+            }
+
+            // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/412
+            // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/pull/413
+            foreach (var parameter in operation.Parameters.OfType<NonBodyParameter>())
+            {
+                var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+
+                if (parameter.Description == null)
+                {
+                    parameter.Description = description.ModelMetadata?.Description;
+                }
+
+                if (parameter.Default == null)
+                {
+                    parameter.Default = description.DefaultValue;
+                }
+
+                parameter.Required |= description.IsRequired;
+            }
+
+            var headerParameter = operation.Parameters.FirstOrDefault(a => a.Name.ToLowerInvariant() == "x-api-version");
+            if (headerParameter == null)
+            {
+                headerParameter = new NonBodyParameter
+                {
+                    Name = "api-version",
+                    In = "header",
+                    Type = "string",
+                    Required = false
+                };
+
+                operation.Parameters.Add(headerParameter);
+            }
+
+            headerParameter.Required = false;
+        }
+    }
+}
